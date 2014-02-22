8e34796bda5736c91bc64a5db2a9be90f8964fd2
Shaun Kruger <shaun.kruger@gmail.com>
2014-02-22 07:18:58 -0700

    Cleanup poll_pos_transactions.

--- coinexchange/btc/pos/management/commands/poll_pos_transactions.py
+++ coinexchange/btc/pos/management/commands/poll_pos_transactions.py
@@ -17,11 +17,11 @@ def find_tx_parent(conn, tx):
     try:
         extra = conn.getrawtransaction(tx.txid)
     except bitcoinrpc.exceptions.InvalidAddressOrKey:
-        print "Invalid tx: %s -> %0.8f" % (tx.txid, tx.amount)
+#         print "Invalid tx: %s -> %0.8f" % (tx.txid, tx.amount)
         return None
-    print "====================="
-    print "TX: %s" % tx
-    print "Raw: %s" % extra
+#     print "====================="
+#     print "TX: %s" % tx.txid
+#     print "Raw: %s" % extra
 #     print "Details: %s" % tx.details
 #     print dir(extra)
 
@@ -29,9 +29,10 @@ def poll_transactions(conn, count=5):
     for tx in conn.listtransactions(count=count):
         tx_rec = agentlib.store_btc_tx(tx)
         if tx.category=="receive":
-            find_tx_parent(conn, tx)
+#             find_tx_parent(conn, tx)
             sales_tx = lib.match_pending_transaction(tx,tx_rec)
             if sales_tx:
+                print "Processing tx: %s" % tx.txid
                 lib.process_btc_transaction(tx, tx_rec, sales_tx)
                 lib.notify_transaction(sales_tx, "confirmed")
 
@@ -42,10 +43,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         conn = bitcoinrpc.connect_to_remote(*BITCOINRPC_ARGS['args'], **BITCOINRPC_ARGS['kwargs'])
         poll_transactions(conn, 50)
-#         return
         try:
             while True:
                  poll_transactions(conn, 5)
-                 time.sleep(5)
+                 time.sleep(3)
+#                  print "Tick..."
         except KeyboardInterrupt:
             print "Keyboard interrupt received.  Exiting."
