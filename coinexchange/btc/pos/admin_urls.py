f710f0af3fdd81c5811e36a9adb29171a67de639
Shaun Kruger <shaun.kruger@gmail.com>
2014-03-05 23:50:18 -0700

    Update UI to remove unneeded pages.

--- coinexchange/btc/pos/admin_urls.py
+++ coinexchange/btc/pos/admin_urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 import coinexchange.account.api.views
 
 urlpatterns = patterns('',
-    url(r'^$', 'coinexchange.btc.pos.views.admin', name='pos_admin'),
+#     url(r'^$', 'coinexchange.btc.pos.views.admin', name='pos_admin'),
     url(r'^merchant_settings$', 'coinexchange.btc.pos.views.merchant_settings', name='pos_merchant_settings'),
     url(r'^batch/(\d+)$', 'coinexchange.btc.pos.views.batch', name='pos_batch'),
     url(r'^make_batch$', 'coinexchange.btc.pos.views.make_batch', name='pos_make_batch'),
